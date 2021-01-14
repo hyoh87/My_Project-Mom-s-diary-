d719b095442ed8e77474d5762c26346380b0c794
hyoh <hyeyong87@gmail.com>
2021-01-14 22:09:09 +0900

    app.py작성 및 template 작성

--- app.py
+++ app.py
@@ -9,12 +9,45 @@ client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑
 db = client.dbmyproject  # 'dbsparta'라는 이름의 db를 만들거나 사용합니다.
 
 
-db.diary_breast.insert_one({'time1': '7:00', 'time2': '7:40', 'location' : '왼쪽', 'memo' : '트름을 10분만에 함'})
-db.diary_breast.insert_one({'time1': '9:00', 'time2': '9:40', 'location' : '오른쪽', 'memo' : '귀저기 갈음(소변)'})
-db.diary_breast.insert_one({'time1': '11:00', 'time2': '11:20', 'location' : '오른쪽', 'memo' : '먹다 잠듬'})
-db.diary_breast.insert_one({'time1': '15:00', 'time2': '15:45', 'location' : '왼쪽', 'memo' : '오래자서 그런지 오랫동안 먹네'})
+@app.route('/')
+def home():
+    return render_template('index.html')
 
+
+## API 역할을 하는 부분
+@app.route('/diary', methods=['POST'])
+def write_diary():
+    # title_receive로 클라이언트가 준 title 가져오기
+    currentDate_receive = request.form['currentDate_give']
+    # author_receive로 클라이언트가 준 author 가져오기
+    startTime_receive = request.form['startTime_give']
+    # review_receive로 클라이언트가 준 review 가져오기
+    endTime_receive = request.form['endTime_give']
+    way_receive = request.form['way_give']
+    comment_receive = request.form['comment_give']
+
+    # DB에 삽입할 review 만들기
+    diary = {
+        'currentDate': currentDate_receive,
+        'startTime': startTime_receive,
+        'endTime': endTime_receive,
+        'way' : way_receive,
+        'comment' : comment_receive
+    }
+    # reviews에 review 저장하기
+    db.diaries.insert_one(diary)
+    # 성공 여부 & 성공 메시지 반환
+    return jsonify({'result': 'success', 'msg': '성공적으로 작성되었습니다.'})
+
+
+@app.route('/diary', methods=['GET'])
+def read_diary():
+    # 1. DB에서 리뷰 정보 모두 가져오기
+    diaries = list(db.diaries.find({}, {'_id': 0}))
+    # 2. 성공 여부 & 리뷰 목록 반환하기
+    return jsonify({'result': 'success', 'diaries': diaries})
 
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
+
